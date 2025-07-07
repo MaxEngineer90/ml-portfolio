01e6d48668c12ec3ab6da057dfb3484775b216a0
lammmax <maximilianlamm.kontakt@outlook.de>
2025-07-07 17:33:53 +0200

    fix: init flowbite

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
+import { initFlowbite } from 'flowbite';
 import { App } from './app/app';
+import { appConfig } from './app/app.config';
 
 bootstrapApplication(App, appConfig)
+  .then(() => {
+    initFlowbite();
+  })
   .catch((err) => console.error(err));
