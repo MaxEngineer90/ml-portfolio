edccaa1a0846d2a5a5de43026a091509eba95ba1
lammmax <maximilianlamm.kontakt@outlook.de>
2025-07-10 15:35:28 +0200

    feat(hero): complete initial hero section implementation

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Hero } from './components/hero/hero';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Hero],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
